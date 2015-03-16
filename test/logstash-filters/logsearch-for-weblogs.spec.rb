171d1da1ab1b9857d2f5c2d987d8a96379370112
David Laing <david@davidlaing.com>
2015-03-16 20:25:40 +0000

    Add apache_combined parsing and remove filters for syslog (which is now bundled with the core logsearch-boshrelease)

--- test/logstash-filters/logsearch-for-weblogs.spec.rb
+++ test/logstash-filters/logsearch-for-weblogs.spec.rb
@@ -10,12 +10,12 @@ describe 'All logsearch-for-weblogs parsers' do
     }
   CONFIG
 
-  context 'nginx_combined log over syslog' do
+  context 'nginx_combined' do
 
-    sample('@type' => 'syslog', '@message' => '<14>1 2015-01-16T15:17:17.046+0000 logsearch-workspace LOGSTASH - - [LOGSTASH@1.4.0 @type="nginx_combined"] 10.10.16.16 - - [16/Jan/2015:15:10:11 +0000] "GET /docs/workspace/develop-the-logsearch-website.html HTTP/1.1" 200 2166 "http://www.logsearch.io/docs/workspace/" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36" 0.000') do
+    sample('@type' => 'nginx_combined', '@message' => '10.10.16.16 - - [16/Jan/2015:15:10:11 +0000] "GET /docs/workspace/develop-the-logsearch-website.html HTTP/1.1" 200 2166 "http://www.logsearch.io/docs/workspace/" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36" 0.000') do
       #puts subject.to_hash.to_yaml
 
-      insist { subject['tags'] } === ["syslog_standard", "nginx"]
+      insist { subject['tags'] } === ["nginx"]
       insist { subject['@type'] } === 'nginx_combined'
       insist { subject['@timestamp'] } == Time.iso8601('2015-01-16T15:10:11.000Z')
 
@@ -25,5 +25,26 @@ describe 'All logsearch-for-weblogs parsers' do
     end
 
   end #context: nginx_combined log over syslog
+
+  context 'apache_combined' do
+    sample('@type'=>'apache_combined', "@message" => '192.168.0.39 - - [31/Jul/2014:07:25:53 -0500] "GET /spread-betting/wp-includes/js/jquery/jquery.js?ver=1.11.0 HTTP/1.1" 200 96402 "http://origin-www.cityindex.co.uk/spread-betting/" "GomezAgent 2.0"') do
+
+      insist { subject['tags'] } === ["apache"]
+
+      insist { subject["@timestamp"] } == Time.iso8601("2014-07-31T12:25:53.000Z")
+
+      insist { subject['clientip'] } == '192.168.0.39'
+      insist { subject['ident'] } == '-'
+      insist { subject['auth'] } == '-'
+      insist { subject['timestamp'] } == '31/Jul/2014:07:25:53 -0500'
+      insist { subject['verb'] } == 'GET'
+      insist { subject['request'] } == '/spread-betting/wp-includes/js/jquery/jquery.js?ver=1.11.0'
+      insist { subject['httpversion'] } === '1.1'
+      insist { subject['response'] } === 200
+      insist { subject['bytes'] } === 96402
+      insist { subject['referrer'] } === "\"http://origin-www.cityindex.co.uk/spread-betting/\""
+      insist { subject['agent'] } == '"GomezAgent 2.0"'
+    end
+  end
 
 end #describe: All logsearch-for-weblogs parsers
